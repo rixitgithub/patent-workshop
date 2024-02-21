47d172eed53fc3dc6a0773e94944a0b8c9ab1cfa
Rishit Tiwari <128586763+rixitgithub@users.noreply.github.com>
2024-02-21 09:45:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hero from "./Hero";
+import Topics from "./Topics";
+import Guest from "./Guest";
 
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Hero />
+      <Topics />
+      <Guest />
+    </>
   );
 }
 
-export default App;
+export default Home;
