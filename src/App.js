c853b6c92cd6557d8f73cb4d233e0fd5ca43c7c6
Rishit Tiwari <128586763+rixitgithub@users.noreply.github.com>
2024-02-22 09:34:12 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Home from "./components/Home";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
-      <Home />
+      <Hero />
     </>
   );
 }
